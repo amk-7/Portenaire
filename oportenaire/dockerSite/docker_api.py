e1c57ea281cb348e6329ef294b8df2222396cc44
amk <abdoulmalikkondi8@gmail.com>
2023-01-25 08:13:52 +0000

    dockerapi conteneur

--- oportenaire/dockerSite/docker_api.py
+++ oportenaire/dockerSite/docker_api.py
@@ -1,40 +1,114 @@
 import docker
 import requests
+#from . import exception as E
+import exception as E
 
 client = docker.from_env()
 
-
 def getContainers():
-    return client.containers.list()
+    containers = client.containers.list(all=True)
+    _containers = []
+    for c in containers:
+        _container = {}
+        _container["name"] = c.name
+        _container["state"] = c.attrs["State"]
+        _container["image"] = "c.image"
+        _container["created_at"] = c.attrs['Created']
+        _container["ip_adresse"] = getKey(c.attrs['NetworkSettings']['Networks'])['IPAddress']
+        _containers.append(_container)
+
+    return _containers
 
 def getImages():
     return client.images.list()
 
+def pullImage(name: str):
+    result = client.images.pull(name)
+    return
+        
+def pushImage(image_id):
+	pass
+
+def findImageByNameAndTagg(image_name, tag_name):
+	pass
+	
+
+
 def getVolumes():
     return client.volumes.list()
 
+#::: Containers ::::::::::::::::::::::::::::::::::::
+def runImage(context: dict) -> bool:
+    result = None
+    try :
+        result = client.containers.run(image=context['image'], command=context['commande'][0], detach=context['detach'])
+    except Exception as e:
+        raise e
+    print(result)
+    return result == None
+
+def removeContainer(container_id: str) -> bool:
+    result = None
+    try:
+        container = findContainerIdByName(container_id)
+        if container:
+            result = container.remove()
+    except Exception as e:
+        raise e
+    return result == None
+
+def stopContainer(container_id):
+    result = None
+    try:
+        container = findContainerIdByName(container_id)
+        if container:
+            result = container.stop()
+    except Exception as e:
+        raise e
+    return result == None
+
+def startContainair(container_id):
+    result = None
+    try:
+        container = findContainerIdByName(container_id)
+        if container:
+            result = container.start()
+    except Exception as e:
+        raise e
+    return result == None
+
+def findContainerIdByName(containe_alpha):
+    result = None
+    try:
+        containers = client.containers.list(all=True)
+        for container in containers:
+            if formatID(container.id) == containe_alpha:
+                result = container
+    except Exception as e:
+        raise e
+    return result
+
+def formatID(id: str) -> str:
+    return id[:12]
+
+def getKey(dicti):
+    for k in dicti :
+        return dicti[k]
+
+def searchImage(value):
+    response = requests.get(f'https://hub.docker.com/api/content/v1/products/search?q={value}')
+    if response.status_code == 200:
+        data = response.json()
+        if data['summaries'] == None:
+            raise E.ImageFromDockerHubNotFound(f'L\'image {value} n\'a pas été trouvé.')
+
+        return data['summaries']
+    else:
+        raise E.ImageFromDockerHubNotFound(f'Erreur de connection code {response.status_code}')
 
 #print(getImages()[0].attrs)
-
-def researchImage(system):
-    nom = 'ubuntu'
-    response = requests.get('https://hub.docker.com/api/content/v1/products/search?q='+nom)
-    # Vérifier que la réponse est correcte
-    if response.status_code == 200:
-        # Charger les données JSON de la réponse
-        data = response.json()
-        # Afficher la liste des images
-        listImage={}
-        for image in data['summaries']:
-            listImage =image['name']
-            #print(image['name'])
-            print(listImage)
-
-        #print(data["summaries"])
-        return listImage
-    else:
-        # Afficher le message d'erreur
-        print('Erreur lors de la récupération des données code : ', response.status_code)
-researchImage('')
-"""for img in getImages():
-    print(img.attrs)"""
+#print(searchImage('debian'))
+#getContainers()
+#runImage({"image": "alpine", "commande":["echo 'hello world'"], "detach" : False})
+#removeContainer("729d418b3b29")
+startContainair("4ea683d1e24f")
--- oportenaire/dockerSite/views.py
+++ oportenaire/dockerSite/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import dockerSite.docker_api as da
-# Create your views here.
+
+def index(request):
+    return render(request, "dockerDesktop/index.html", context={})
+
+def addContainer(request):
+    return render(request, "dockerDesktop/add_container.html", context={})
+
 def showVolumes(request):
     volumes = [1,2,3]
-    return render(request,"docker/volume.html",{"volumes":volumes})
+    return render(request,"dockerDesktop/volume.html",{"volumes":volumes})
 
 def showContainers(request):
     containers = da.getContainers()
-    print(containers)
-    return render(request,"docker/containers.html",{"containers":containers})
+    #print(containers[0].attrs)
+    return render(request,"dockerDesktop/containers.html",{"containers":containers})
 
 def showImages(request):
     images = da.getImages()
-    return render(request,"docker/image.html",{"images":images})
+    return render(request,"dockerDesktop/image.html",{"images":images})
+
+def RunImage(request):
+    da.runImage({})
+    return redirect('/dockerSite/containers/')
+
+def searchImageDockerHub(request):
+    images = []
+    if request.method == "POST":
+        images = da.searchImage('debian')
+    print(images[0])
+    return render(request, "dockerHub/search_image.html", context={'images' : images})
