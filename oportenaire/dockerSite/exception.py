f96290c1d5f155f34316c28a8c90f87f979787c8
adjanayo <sadjanayo396@gmail.com>
2023-02-28 09:08:18 +0000

    doc

--- oportenaire/dockerSite/exception.py
+++
@@ -1,7 +0,0 @@
-
-class ImageFromDockerHubNotFound(Exception):
-    def __init__(self, _message: str) -> None:
-        self.message = _message
-        super().__init__(self.message)
-    def __str__(self):
-        return self.message
